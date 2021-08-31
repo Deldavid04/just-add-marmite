f3afe7ecb5f26b043e54cbbbd2c235292ddfbc8f
Deldavid04 <76662035+Deldavid04@users.noreply.github.com>
2021-08-31 04:50:31 +0100

    testing git

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,38 @@
-export default function Recipes() {
+import { createClient } from 'contentful';
+import RecipeCard from '../components/RecipeCard';
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: 'ck7byh7zx03w',
+    accessToken: 'dkLo14cQQ3XpJ1lM1ph5tKp2CpI4cRY40ShA0xNzrHs',
+  });
+
+  const res = await client.getEntries({ content_type: 'recipe' });
+
+  return {
+    props: {
+      recipes: res.items,
+    },
+  };
+}
+
+export default function Recipes({ recipes }) {
+  console.log(recipes);
   return (
-    <div className="recipe-list">
-      Recipe List
+    <div className='recipe-list'>
+      {recipes.map((recipe) => (
+        <RecipeCard key={recipe.sys.id} recipe={recipe} />
+      ))}
+
+      <style jsx>
+        {`
+          .recipe-list {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-gap: 20px 60px;
+          }
+        `}
+      </style>
     </div>
-  )
+  );
 }
